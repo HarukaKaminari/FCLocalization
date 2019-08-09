fad50522c688b183a7b7de2cdd203cb826ceaa88
Haruka <senriharuka@hotmail.com>
2019-08-09 13:06:09 +0800

    Update Common.cs

--- Mappy Kids/Tools/Common.cs
+++ Mappy Kids/Tools/Common.cs
@@ -40,6 +40,65 @@ namespace TranslationOrganizer
         }
 
         /// <summary>
+        /// 读ROM文件，获得CHR数据
+        /// </summary>
+        /// <param name="fileName">ROM文件名</param>
+        /// <returns>CHR数据</returns>
+        public static Byte[] GetCHRData(string fileName)
+        {
+            Byte[] chrData = null;
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            if (fs != null)
+            {
+                BinaryReader br = new BinaryReader(fs);
+                if (br != null)
+                {
+                    Byte[] header = br.ReadBytes(16);
+                    int prgSize = header[4] * 16 * 1024;
+                    int chrSize = header[5] * 8 * 1024;
+                    br.ReadBytes(prgSize);
+                    chrData = br.ReadBytes(chrSize);
+
+                    br.Close();
+                    br.Dispose();
+                    br = null;
+                }
+                fs.Close();
+                fs.Dispose();
+                fs = null;
+            }
+            return chrData;
+        }
+
+        /// <summary>
+        /// 给ROM打补丁。要配合GetPRGData和GetCHRData这两个函数使用
+        /// </summary>
+        /// <param name="fileName">ROM文件名</param>
+        /// <param name="prgData">PRG补丁数据。如果为null的话则忽略给PRG打补丁</param>
+        /// <param name="chrData">CHR补丁数据。如果为null的话则忽略给CHR打补丁</param>
+        public static void PatchROM(string fileName, Byte[] prgData, Byte[] chrData)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            if(fs != null)
+            {
+                BinaryWriter bw = new BinaryWriter(fs);
+                if(bw != null)
+                {
+                    bw.Seek(16, SeekOrigin.Begin);
+                    bw.Write(prgData);
+                    bw.Write(chrData);
+                    bw.Flush();
+                    bw.Close();
+                    bw.Dispose();
+                    bw = null;
+                }
+                fs.Close();
+                fs.Dispose();
+                fs = null;
+            }
+        }
+
+        /// <summary>
         /// 读TBL文件，获得码表
         /// </summary>
         /// <param name="fileName">码表文件名</param>
