bd1007783d7d9b607f3fd6eb0a2146ddebf6a754
Haruka <senriharuka@hotmail.com>
2019-08-09 13:07:14 +0800

    Update TileCreator.cs

--- Mappy Kids/Tools/TileCreator.cs
+++ Mappy Kids/Tools/TileCreator.cs
@@ -50,7 +50,7 @@ namespace TranslationOrganizer
             {
                 for(int _x = 0; _x < 8; ++_x)
                 {
-                    int c = img.GetPixel(x * 8 + _x, y * 8 + _y).ToArgb();
+                    int c = img.GetPixel(x * 8 + _x, y * 8 + _y).ToArgb() & 0xFFFFFF;
                     pixels.Add(c);
                 }
             }
@@ -76,7 +76,7 @@ namespace TranslationOrganizer
                     int idx = 0;
                     for(idx = 0; idx < VALID_COLORS.Length; ++idx)
                     {
-                        if(c == VALID_COLORS[idx])
+                        if(c == (VALID_COLORS[idx] & 0xFFFFFF))
                         {
                             break;
                         }
@@ -87,7 +87,7 @@ namespace TranslationOrganizer
                     // 将颜色索引分解成两个bit，然后分别存入两个plane
                     for(int i = 0; i < 2; ++i)
                     {
-                        plane[i] |= (Byte)(((idx >> i) & 1) << x);
+                        plane[i] |= (Byte)(((idx >> i) & 1) << (7 - x));
                     }
                 }
                 data0.Add(plane[0]);
@@ -106,6 +106,68 @@ namespace TranslationOrganizer
         private static bool isMultipleOf8(int value)
         {
             return value / 8 * 8 == value;
+        }
+
+        public static void Test()
+        {
+            string ROMFileName = "D:/Mesen-0.9.8/bin/x64/Release/Roms/Mappy Kids (J).nes";
+            // 生成一张图
+            Bitmap canvas = new Bitmap(128, 64, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(canvas);
+            Bitmap charImg = new Bitmap(16, 16, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Graphics charG = Graphics.FromImage(charImg);
+            charG.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+            string str = "你很努力嘛！想要什么礼物吗？";
+            Font font = new Font("宋体", 9);
+            Brush brush = new SolidBrush(Color.FromArgb(255, 255, 255));
+            int x = 0, y = 0;
+            foreach(char c in str)
+            {
+                charG.Clear(Color.FromArgb(0, 0, 0));
+                charG.DrawString(c.ToString(), font, brush, new Point(0, 0));
+                g.DrawImage(charImg, new Rectangle(x + 0, y + 0, 8, 8), new Rectangle(0, 0, 8, 8), GraphicsUnit.Pixel);
+                g.DrawImage(charImg, new Rectangle(x + 8, y + 0, 8, 8), new Rectangle(0, 8, 8, 8), GraphicsUnit.Pixel);
+                g.DrawImage(charImg, new Rectangle(x + 16, y + 0, 8, 8), new Rectangle(8, 0, 8, 8), GraphicsUnit.Pixel);
+                g.DrawImage(charImg, new Rectangle(x + 24, y + 0, 8, 8), new Rectangle(8, 8, 8, 8), GraphicsUnit.Pixel);
+                x += 32;
+                if(x >= 128)
+                {
+                    x = 0;
+                    y += 8;
+                }
+            }
+            // 生成PatternTable
+            Byte[] data = Bitmap2Data(canvas);
+            // 写入CHR Bank $80
+            Byte[] chrData = Common.GetCHRData(ROMFileName);
+            for(int i = 0; i < data.Length; ++i)
+            {
+                chrData[0x20000 + i] = data[i];
+            }
+            // 修改文本
+            Byte[] prgData = Common.GetPRGData(ROMFileName);
+            int addr = TableOrganizer.GetAbsoluteAddress(0xB5A5);
+            prgData[addr + 0] = 0xF1;
+            prgData[addr + 1] = 0x20;
+            prgData[addr + 2] = 0x60;
+            prgData[addr + 3] = 0x61;
+            prgData[addr + 4] = 0x62;
+            prgData[addr + 5] = 0x63;
+            prgData[addr + 6] = 0x64;
+            prgData[addr + 7] = 0x65;
+            prgData[addr + 8] = 0xF0;
+            prgData[addr + 9] = 0x66;
+            prgData[addr + 10] = 0x67;
+            prgData[addr + 11] = 0x68;
+            prgData[addr + 12] = 0x69;
+            prgData[addr + 13] = 0x6A;
+            prgData[addr + 14] = 0x6B;
+            prgData[addr + 15] = 0x6C;
+            prgData[addr + 16] = 0x6D;
+            prgData[addr + 17] = 0xF2;
+            prgData[addr + 18] = 0xF1;
+            // 给ROM打补丁
+            Common.PatchROM(ROMFileName, prgData, chrData);
         }
     }
 }
